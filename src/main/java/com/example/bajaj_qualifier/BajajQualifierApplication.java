0a27fc1ed0a2517170a4b244532afdf32e0d3b0c
Kushagra Agarwal <kushagraagarwal2003@gmail.com>
2025-11-11 16:02:41 +0530

    Initial commit

---
+++ src/main/java/com/example/bajaj_qualifier/BajajQualifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.bajaj_qualifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajQualifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajQualifierApplication.class, args);
+	}
+
+}
